d18de92fa034e45c0b746368274d659dac79d471
Vitaliy Blagov <vitaliy@blagov.com>
2019-05-17 11:26:57 +0300

    Store initial settings

--- src/store.js
+++ src/store.js
@@ -5,12 +5,33 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    charactersData: [],
+    pagination: {
+      currentPage: 0,
+      perPage: 20,
+      totalRecords: 0
+    }
   },
   mutations: {
-
+    SET_CHARACTERS: (state, characters) => {
+      state.charactersData = characters
+    },
+    SET_PAGINATION_PARAMETER: (state, paginationSetting) => {
+      state.pagination = { ...state.pagination, ...paginationSetting }
+    }
   },
   actions: {
-
+    setCharacters ({ commit }, characters) {
+      commit('SET_CHARACTERS', characters.map((character, index) => ({ ...character, ...{ id: index } })))
+    },
+    setPagination ({ commit }, paginationSettings) {
+      commit('SET_CHARACTERS', paginationSettings)
+    }
+  },
+  getters: {
+    getSingleCharacter: state => id => state.charactersData.filter(character => character.id === id),
+    getPaginationSetting: state => key => state.pagination[key],
+    getPaginationSettins: state => state.pagination
   }
+
 })
